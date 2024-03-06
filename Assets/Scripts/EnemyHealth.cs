59052ebd5e96d1b8bec25229f121aa42ba749023
itouriku <116050837+itouriku@users.noreply.github.com>
2024-03-06 21:05:10 +0900

    EnemyHealth追加

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 10; //エネミーの最大ヘルス
+    public int currentHealth;　//現在のヘルス
+
+    void Start()
+    {
+        currentHealth = maxHealth; //最大ヘルスで初期化
+    }
+
+    void Update()
+    {
+
+    }
+}
