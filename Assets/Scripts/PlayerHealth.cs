bb7b214073eaa10d34fc9ee20686e968e0c4e683
nun36 <kaname@lagoon.ocn.ne.jp>
2024-03-08 22:27:21 +0900

    PlayerHealthの中に、エネミーに接触したときのダメージ処理を追加

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,17 +5,52 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
 
-    public int health = 100;
+    public int maxHealth = 100;  //体力の最大値
+    private int nowHealth;　     //現在の体力
+
+    private bool isDamaged = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxHealth = nowHealth;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            if (!isDamaged)
+            {
+                isDamaged = true;
+                InvokeRepeating("TakeDamage", 0.5f, 0.5f); // 0.5秒ごとにTakeDamageメソッドを呼び出す
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            CancelInvoke("TakeDamage"); // ダメージを受けなくなったらInvokeをキャンセルする
+            isDamaged = false;
+        }
+    }
+
+    private void TakeDamage()
+    {
+        nowHealth -= 5; // 体力を減らす
+
+        if (nowHealth <= 0)
+        {
+
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
