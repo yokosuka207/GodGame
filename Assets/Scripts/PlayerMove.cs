71aa478ee00985aed3cf306ac6af73ba4f05847c
Mazaki0314 <mazaki177@gmail.com>
2024-03-18 16:07:27 +0900

    地面判定追加

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -58,6 +58,6 @@ public class PlayerMove : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, 0.0f);
         }
 
-        Debug.Log(rb.velocity);
+        //Debug.Log(rb.velocity);
     }
 }
