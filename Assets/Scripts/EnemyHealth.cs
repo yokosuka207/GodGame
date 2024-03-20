cc312c8ad730e42e4c0b4312e10471f4ce497bc2
itouriku <116050837+itouriku@users.noreply.github.com>
2024-03-20 16:55:23 +0900

    被ダメージ

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -7,13 +7,40 @@ public class EnemyHealth : MonoBehaviour
     public int maxHealth = 10; //エネミーの最大ヘルス
     public int currentHealth; //現在のヘルス
 
+    bool isBlockCollided = false;
+    bool isFlameCollided = false;
+
     void Start()
     {
         currentHealth = maxHealth; //最大ヘルスで初期化
     }
 
-    void Update()
+    void OnTriggerStay2D(Collider2D other)
     {
 
+        // 衝突したオブジェクトがオブジェクト2(Block)である場合
+        if (other.gameObject.CompareTag("Block"))
+        {
+            isBlockCollided = true;
+        }
+        if (other.gameObject.CompareTag("Flame"))
+        {
+            isFlameCollided = true;
+        }
+
+    }
+
+    void Update()
+    {
+        if (isBlockCollided && isFlameCollided)
+        {
+
+            DealDamage();
+        }
+    }
+
+    void DealDamage()
+    {
+        currentHealth -= 10;
     }
 }
