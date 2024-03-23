97199a95fb5d9fc06aa55143ad55e244d2e6dfa3
itouriku <116050837+itouriku@users.noreply.github.com>
2024-03-23 22:42:30 +0900

    Enemy死亡処理

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public int maxHealth = 10; //エネミーの最大ヘルス
-    public int currentHealth; //現在のヘルス
+    public int maxHealth = 10; // エネミーの最大ヘルス
+    public int currentHealth; // 現在のヘルス
 
     bool isBlockCollided = false;
     bool isFlameCollided = false;
 
     void Start()
     {
-        currentHealth = maxHealth; //最大ヘルスで初期化
+        currentHealth = maxHealth; // 最大ヘルスで初期化
     }
 
     void OnTriggerStay2D(Collider2D other)
     {
-
         // 衝突したオブジェクトがオブジェクト2(Block)である場合
         if (other.gameObject.CompareTag("Block"))
         {
@@ -27,20 +26,36 @@ public class EnemyHealth : MonoBehaviour
         {
             isFlameCollided = true;
         }
-
     }
 
     void Update()
     {
         if (isBlockCollided && isFlameCollided)
         {
-
             DealDamage();
         }
     }
 
     void DealDamage()
     {
-        currentHealth -= 10;
+        currentHealth -= 1;
+        if (currentHealth <= 0)
+        {
+            Die(); // ヘルスが0以下になったらDieメソッドを呼び出す
+        }
     }
+
+    void Die()
+    {
+        // エネミーを消す
+        Destroy(gameObject);
+
+        // プレイヤーにエネミーが死亡したことを通知する
+        PlayerHealth player = FindObjectOfType<PlayerHealth>();
+        if (player != null)
+        {
+            player.EnemyDied();
+        }
+    }
+
 }
